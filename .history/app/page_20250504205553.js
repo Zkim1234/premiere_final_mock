c9d2c36f5de776d4ff1af08ad558fd1ed3e245ed
Seoyoung Kim <zinna@Zinna-Kim.local>
2025-05-04 21:06:21 -0700

    testing font to onboarding

---
+++ .history/app/page_20250504205553.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    Find Fun 
+and 
+Save More!
+   </div>
+  );
+}
---
+++ .history/app/page_20250504205558.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    Find Fun and Save More!
+   </div>
+  );
+}
