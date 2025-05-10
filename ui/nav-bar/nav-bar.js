441ddab8b85879c08707e4ba83dee822cf369615
Denise Aquino <daquino10@my.bcit.ca>
2025-05-10 08:41:41 -0700

    add nav bar files and notfi reminder

---
+++ ui/nav-bar/nav-bar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "@/ui/nav-bar/nav-bar.module.css";
+
+export default function NavBar() {
+  return (
+    <div className={styles.navContainer}>
+
+    </div> )
