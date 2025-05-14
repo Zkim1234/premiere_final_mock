0454c2934fd133ccb01edb38a720ae82e721bd8f
Denise Aquino <daquino10@my.bcit.ca>
2025-05-13 20:23:09 -0700

    add settings and accessibility

--- app/settings-page/page.js
+++ app/settings-page/page.js
@@ -7,63 +7,63 @@ import NavBar from "@/ui/nav-bar/nav-bar";
 export default function SettingsPage() {
   return (
     <div className={styles.pageContainer}>
-        <div>
-            <h1 className={styles.settingsTitle}>Settings</h1>
-        </div>
+      <div>
+        <h1 className={styles.settingsTitle}>Settings</h1>
+      </div>
 
-        <div className={styles.settingsContainer}>
-            <div>
-                <SettingsButton
-                image="settingsIcons\accessible_forward.svg"
-                alt="accessibility"
-                text="Accessibility Settings"
-                arrow="\Arrow_down.svg"
-                arrowAlt="arrow"
-                />
-            </div>
-            <div>
-                <SettingsButton
-                image="settingsIcons\ic24-unlock.svg"
-                alt="privacy"
-                text="Privacy & Data"
-                arrow="\Arrow_down.svg"
-                arrowAlt="arrow"
-                />
-            </div>
-            <div>
-                <SettingsButton
-                image="settingsIcons\ic24-help.svg"
-                alt="help"
-                text="Help Center"
-                arrow="\Arrow_down.svg"
-                arrowAlt="arrow"
-                />
-            </div>
-            <div>
-                <SettingsButton
-                image="settingsIcons\Info.svg"
-                alt="about"
-                text="About"
-                arrow="\Arrow_down.svg"
-                arrowAlt="arrow"
-                />
-            </div>
-            <div>
-                <SettingsButton
-                image="settingsIcons\file_copy.svg"
-                alt="terms"
-                text="Terms & Conditions "
-                arrow="\Arrow_down.svg"
-                arrowAlt="arrow"
-                />
-            </div>
+      <div className={styles.settingsContainer}>
+        <div>
+          <SettingsButton
+            image="settingsIcons\accessible_forward.svg"
+            alt="accessibility"
+            text="Accessibility Settings"
+            arrow="\Arrow_down.svg"
+            arrowAlt="arrow"
+          />
         </div>
         <div>
-            <h4 className={styles.deleteAccount}>Delete Account</h4>
+          <SettingsButton
+            image="settingsIcons\ic24-unlock.svg"
+            alt="personalization"
+            text="Personalization"
+            arrow="\Arrow_down.svg"
+            arrowAlt="arrow"
+          />
         </div>
         <div>
-            <NavBar />
+          <SettingsButton
+            image="settingsIcons\ic24-help.svg"
+            alt="help"
+            text="Help Center"
+            arrow="\Arrow_down.svg"
+            arrowAlt="arrow"
+          />
         </div>
+        <div>
+          <SettingsButton
+            image="settingsIcons\Info.svg"
+            alt="about"
+            text="About"
+            arrow="\Arrow_down.svg"
+            arrowAlt="arrow"
+          />
+        </div>
+        <div>
+          <SettingsButton
+            image="settingsIcons\file_copy.svg"
+            alt="terms"
+            text="Terms & Conditions "
+            arrow="\Arrow_down.svg"
+            arrowAlt="arrow"
+          />
+        </div>
+      </div>
+      <div>
+        <h4 className={styles.deleteAccount}>Delete Account</h4>
+      </div>
+      <div>
+        <NavBar />
+      </div>
     </div>
   );
 }
