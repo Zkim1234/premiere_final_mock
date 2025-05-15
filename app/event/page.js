65c2c12b412e90ebfb370608aa803b5f1fe70a00
Micaela <micaela.valmores@gmail.com>
2025-05-15 11:49:34 -0700

    place the carousel function component

--- app/event/page.js
+++ app/event/page.js
@@ -1,7 +1,18 @@
+import EventCard from '@/ui/full-sized-event-card/full-sized-event-card';
+import style from '@/app/event/event.module.css';
+import SmallCard from '@/ui/carousel-card/Small-card';
 
-import EventCard from '@/ui/full-sized-event-card/full-sized-event-card'
-import style from '@/app/event/event.module.css'
-
+export function CarouselComponent() {
+  return (
+    <div>
+      <SmallCard
+        image="/example.jpg"
+        title="Sample Title"
+        description="This is a description."
+      />
+    </div>
+  );
+}
 
 const PopularEvents = () => {
   return (
@@ -9,24 +20,22 @@ const PopularEvents = () => {
       <EventCard
         image="/images/art_vancouver.jpg"
         title="Art Vancouver 2025"
-        subtitle="Vancouver Convention Centre East
-Thu, Apr 24, 2025 6:00 PM - Sun, Apr 27, 2025 5:00 PM "
+        subtitle={`Vancouver Convention Centre East
+Thu, Apr 24, 2025 6:00 PM - Sun, Apr 27, 2025 5:00 PM `}
       />
       <EventCard
         image="/images/banana_banner.png"
-        title="This is Bananas! 20th Anniversary "
+        title="This is Bananas! 20th Anniversary"
         subtitle="Celebration Fundraiser"
       />
       <EventCard
         image="/images/swimming_banner.png"
         title="Pedalheads Swim Open House - Fremont"
-        subtitle="Pedalheads Swim | Fremont Village
-Fri, Apr 18, 2025 9:00 AM - 2:00 PM PDT"
+        subtitle={`Pedalheads Swim | Fremont Village
+Fri, Apr 18, 2025 9:00 AM - 2:00 PM PDT`}
       />
     </div>
   );
 };
 
 export default PopularEvents;
-
-
